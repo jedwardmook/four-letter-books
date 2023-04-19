107b7eca337cc8a534ddd8808977c4065cdf2bca
JedwardMook <jedwardmook@yahoo.com>
2023-04-19 17:33:49 -0500

    Starts to setup fetch for ISBN but error with cors needs to be figured out

--- client/src/AddBook.js
+++ client/src/AddBook.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SearchBook from './SearchBook'
 
 function AddBook() {
+  const[isbn, setIsbn] = useState('')
+  const[title, setTitle] = useState('')
     //component for adding books
   return (
     <div>
-        <SearchBook />
+        <SearchBook 
+          placeholder="ISBN"
+          value={isbn}
+          onChange={(e) => setIsbn(e.target.value)}
+          fetchAddress="http://openlibrary.org/api/volumes/brief/isbn/"
+          />
+        <SearchBook 
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          />
     </div>
   )
 }
--- client/src/SearchBook.js
+++ client/src/SearchBook.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function SearchBook() {
+function SearchBook({placeholder, value, onChange, fetchAddress}) {
+  const [book, setBook] = useState('')
+
+  function submitSearch(e){
+    e.preventDefault()
+    fetch('http://openlibrary.org/api/volumes/brief/isbn/9780192832696.json')
+      .then((response) => response.json())
+      .then((data) => setBook(data));
+
+      console.log(book)
+    };
+
   return (
-    <input/>
+    <form onSubmit={submitSearch}>
+      <input
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+      <button>Search</button>
+    </form>
   )
 }
 
