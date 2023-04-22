54f64390cfaafb369181cbd459f787d82fd241af
JedwardMook <jedwardmook@yahoo.com>
2023-04-21 20:11:19 -0500

    Removes title input from AddBook, sets author into state from book in state

--- client/src/AddBook.js
+++ client/src/AddBook.js
@@ -3,8 +3,8 @@ import SearchBook from './SearchBook'
 
 function AddBook() {
   const[isbn, setIsbn] = useState()
-  const[title, setTitle] = useState('')
-    //component for adding books
+    
+  //component for adding books
   return (
     <div>
         <SearchBook 
@@ -12,11 +12,6 @@ function AddBook() {
           value={isbn}
           onChange={(e) => setIsbn(e.target.value)}
           fetchAddress="http://openlibrary.org/isbn/"
-          />
-        <SearchBook 
-          placeholder="Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
           />
     </div>
   )
--- client/src/SearchBook.js
+++ client/src/SearchBook.js
@@ -2,13 +2,32 @@ import React, { useState } from 'react'
 
 function SearchBook({placeholder, value, onChange, fetchAddress}) {
   const [book, setBook] = useState()
+  const [author, setAuthor] = useState()
 
   function submitSearch(e){
     e.preventDefault()
     fetch(`${fetchAddress}${value}.json`)
       .then(response => response.json())
-      .then((data) => setBook(data));
+      .then((data) => {
+        setBook(data)
+        console.log(data)
+        splitAuthor(data)});
     };
+
+  function splitAuthor(book){
+    const author = Object.values(book.authors[0])
+    console.log(author)
+    fetchAuthor(author)
+  }
+
+  function fetchAuthor(author){
+    fetch(`http://openlibrary.org${author}.json`)
+    .then(response => response.json())
+      .then((data) => {
+        setAuthor(data)
+        console.log(data)
+      })
+  }
 
   return (
     <form onSubmit={submitSearch}>
@@ -19,6 +38,7 @@ function SearchBook({placeholder, value, onChange, fetchAddress}) {
       />
       <button>Search</button>
       {book&& <h1>{book.title}</h1>}
+      {author&& <h3>{author.name}</h3>}
     </form>
   )
 }
