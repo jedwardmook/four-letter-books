af3870fd3f986c7842c843c43c791ece57fedad0
JedwardMook <jedwardmook@yahoo.com>
2023-04-24 16:12:02 -0500

    Adds state to form inputs

--- client/src/SearchBook.js
+++ client/src/SearchBook.js
@@ -1,9 +1,17 @@
 import React, { useState } from 'react'
 
 function SearchBook({placeholder, value, onChange, fetchAddress}) {
-  const [book, setBook] = useState()
+  const [book, setBook] = useState('')
   const [author, setAuthor] = useState()
-  const [formDiv, setFormDiv] = useState(false)
+  const [isbnFormDiv, setIsbnFormDiv] = useState(false)
+  const [title, setTitle] = useState('')
+  const [subtitle, setSubtitle] = useState('')
+  const [authorName, setAuthorName] = useState('')
+  const [publisher, setPublisher] = useState('')
+  const [publishDate, setPublishDate] = useState('')
+  const [physicalFormat, setPhysicalFormat] = useState('')
+  const [price, setPrice] = useState('')
+
 
   function submitSearch(e){
     e.preventDefault()
@@ -13,7 +21,7 @@ function SearchBook({placeholder, value, onChange, fetchAddress}) {
         setBook(data)
         console.log(data)
         splitAuthor(data)
-        setFormDiv(true)});
+        setIsbnFormDiv(true)});
     };
 
   function splitAuthor(book){
@@ -32,7 +40,7 @@ function SearchBook({placeholder, value, onChange, fetchAddress}) {
   }
 
   function showAddForm(){
-    setFormDiv(!formDiv)
+    setIsbnFormDiv(!isbnFormDiv)
   }
 
   return (
@@ -46,32 +54,36 @@ function SearchBook({placeholder, value, onChange, fetchAddress}) {
         <button>Search</button>
       </form>
       <button onClick={showAddForm}>Add Book Manually</button>
-      {formDiv&&
+      {isbnFormDiv&&
       <div>
         <form>
           <input
             placeholder='Title'
-            value={book&& book.title}
+            value={book? book.title: title}
           />
           <input
             placeholder='Subtitle'
-            value={book&& book.subtitle}
+            value={book? book.subtitle: subtitle}
           />
           <input
             placeholder='Author'
-            value={author&& author.name}
+            value={author? author.name: authorName}
           />
           <input
             placeholder='Publisher'
-            value={book&& book.publishers}
+            value={book? book.publishers: publisher}
           />
           <input
             placeholder='Published Date'
-            value={book&& book.publish_date}
+            value={book? book.publish_date: publishDate}
           />
           <input
             placeholder='Format'
-            value={book&& book.physical_format}
+            value={book? book.physical_format: physicalFormat}
+          />
+          <input
+            placeholder='Price'
+            value={book? book.price: price}
           />
         </form>
       </div>}
