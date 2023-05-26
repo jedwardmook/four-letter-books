e683e43033bd3993a5b0676697f7f4b8104f1445
JedwardMook <jedwardmook@yahoo.com>
2023-05-26 18:10:03 -0500

    Updates SignIn two inputs

--- client/src/SignIn.js
+++ client/src/SignIn.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 function SignIn() {
   return (
-    <div>SignIn</div>
+    <>
+        <form>
+            <input />
+            <input />
+        </form>
+    </>
   )
 }
 
