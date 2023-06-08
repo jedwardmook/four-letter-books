7854057c9fef5790295662967a606a6d6e3124c1
JedwardMook <jedwardmook@yahoo.com>
2023-06-07 23:21:48 -0500

    Adds state to SignUpForm for username, password, password confirmation

--- client/src/SignUpForm.js
+++ client/src/SignUpForm.js
@@ -1,15 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function SignUpForm() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfrimation, setPasswordConfirmation] = useState('')
+
+  const handleUsername = (e) => {
+    setUsername(e.target.value)
+  }
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handlePasswordConfirmation = (e) => {
+    setPasswordConfirmation(e.target.value)
+  }
+
   return (
     <>
         <form>
-          <label>Username</label>
-            <input />
-          <label>Password</label>
-            <input />
-          <label>Password Confirmation</label>
-            <input />
+          <label htmlFor='username'>Username</label>
+            <input 
+              id='username'
+              value={username}
+              onChange={handleUsername}
+              />
+          <label htmlFor='password'>Password</label>
+            <input 
+              id='password'
+              value={password}
+              onChange={handlePassword}
+              />
+          <label htmlFor='passwordConfirmation'>Password Confirmation</label>
+            <input 
+              id='passwordConfirmation'
+              value={passwordConfrimation}
+              onChange={handlePasswordConfirmation}
+              />
         </form>
     </>
   )
