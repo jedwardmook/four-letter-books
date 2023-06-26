8eeb79988beeff719ab3ae496bc95907fa3da903
JedwardMook <jedwardmook@yahoo.com>
2023-06-26 17:53:32 -0500

    installs react router dom and sets up initial routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import AddBook from './AddBook';
 import './App.css';
 import LoginForm from './LoginForm';
@@ -7,11 +8,21 @@ import SignUpForm from './SignUpForm';
 function App() {
 
   return (
-    <div className="App">
-      <AddBook/>
-      <LoginForm />
-      <SignUpForm />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path='login'
+            element= {<LoginForm />}
+            />
+          <Route exact path='login'
+            element= {<SignUpForm />}
+            /> 
+          <Route exact path='login'
+            element={<AddBook/>}
+            />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
