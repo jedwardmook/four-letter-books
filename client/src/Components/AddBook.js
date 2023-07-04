4ea3883261ff07fa30e35090701c251f8cfcfb2c
JedwardMook <jedwardmook@yahoo.com>
2023-07-03 22:29:35 -0500

    AddBook layout changes

--- client/src/Components/AddBook.js
+++ client/src/Components/AddBook.js
@@ -8,12 +8,12 @@ function AddBook() {
   //component for adding books
   return (
     <div>
-        <Link to='/add_book_isbn'>
-         <button>Add book using ISBN</button>
-        </Link>  
         <Link to="/add_book_form">
           <button>Add Book Manually</button>
         </Link>
+        <Link to='/add_book_isbn'>
+         <button>Add Book Using ISBN</button>
+        </Link>  
     </div>
   )
 }
