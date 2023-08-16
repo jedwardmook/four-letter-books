8f0b3905eb012a339f27a6f724402f365237525c
JedwardMook <jedwardmook@yahoo.com>
2023-08-16 14:51:33 -0500

    adds functionality to limiting amount of books displayed on table, starts BookPage

--- client/src/Components/BooksMain.js
+++ client/src/Components/BooksMain.js
@@ -3,34 +3,45 @@ import '../Styles/booksmain.min.css'
 
 
 function BooksMain() {
-    const [books, setBooks] = useState()
+  const [books, setBooks] = useState()
+  const [startIndex, setStartIndex] = useState(0)
+  const [endIndex, setEndIndex] = useState(20)
 
-    useEffect(() => {
-        fetch('/books')
-            .then((response) => {
-                if (response.ok) {
-                response.json().then((books) => {
-                    setBooks(books)
-                    });
-                } else {
-                }
-            })
-    }, [])
+  useEffect(() => {
+      fetch('/books')
+          .then((response) => {
+              if (response.ok) {
+              response.json().then((books) => {
+                  setBooks(books)
+                  });
+              } else {
+              }
+          })
+  }, [])
 
 
 
-    const booksToDisplay = books && books.map((book,index) => {
-      return <tr key={index}>
-              <td>{book.title}</td>
-              <td>{book.author}</td>
-              <td>{book.isbn_10}</td>
-              <td>{book.isbn_13}</td>
-              <td>{book.page_number}</td>
-              <td>{book.condition}</td>
-              <td>{book.price}</td>
-            </tr>
+  const booksToDisplay = books && books.slice(startIndex, endIndex).map((book,index) => {
+    return <tr className="books_main_data" key={index}>
+            <td>{book.title}</td>
+            <td>{book.author}</td>              
+            <td>{book.isbn_10}</td>
+            <td>{book.isbn_13}</td>
+            <td>{book.page_number}</td>
+            <td>{book.condition}</td>
+            <td>{book.price}</td>
+          </tr> 
     })
 
+  function nextPage(){
+    setStartIndex(startIndex + 20)
+    setEndIndex(endIndex + 20)
+  }  
+
+  function lastPage(){
+    setStartIndex(startIndex - 20)
+    setEndIndex(endIndex - 20)
+  } 
   return ( 
       <main className="books_main_main">
         <h2>Books Archived</h2>
@@ -50,6 +61,12 @@ function BooksMain() {
             {booksToDisplay}
           </tbody>
         </table>
+        {booksToDisplay&&
+        <div className="books_main_controls_div">
+          {startIndex > 0 &&<button onClick={lastPage} className="books_main_controls_button">&lt;</button>}
+            <p className="books_main_controls_display">Showing: {startIndex} - {startIndex + booksToDisplay.length}</p>
+          {booksToDisplay.length > endIndex - 1 &&<button onClick={nextPage} className="books_main_controls_button">&gt;</button>}
+        </div>}
     </main>
   )
 }
