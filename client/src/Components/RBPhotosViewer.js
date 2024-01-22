f8fc1a9f4164c11acb2b7617d274463778bb6e55
JedwardMook <jedwardmook@yahoo.com>
2024-01-22 17:01:24 -0600

    Update RBPhotosViewer.js
    Starting logic for heroPhoto

--- client/src/Components/RBPhotosViewer.js
+++ client/src/Components/RBPhotosViewer.js
@@ -4,14 +4,15 @@ function RBPhotosViewer({photos}) {
     const [heroPhoto, setHeroPhoto] = useState(photos[0])
 
     const photosToDisplay = photos?.map((photo) => {
-        return <img src={photo} alt={photo}/>
+        return <img key={photo} src={photo} alt={photo}/>
     })
 
-    console.log(heroPhoto)
+    
 
   return (
     <div>
         {photos.length > 0? photosToDisplay : <img src='https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg' alt='Not available'/>}
+        {photos.length > 0? <img src={heroPhoto} alt='book'/> : <img src='https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg' alt='Not available'/>}
     </div>
   )
 }
